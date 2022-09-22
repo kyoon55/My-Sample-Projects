b393939e2d15a522a64b1bf0b79f6818a092c332
kyoon55 <kyoon5@gwu.edu>
2022-09-22 00:17:01 -0400

    test

--- Java/test.java
+++ Java/test.java
@@ -1,5 +1,16 @@
-class HelloPrinter {
+public class test {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		double[] data = {1, 4, 3, 50};
+		double largest = data[0];
+		for (int i = 1; i < data.length; i++)
+		{
+		  if (data[i] > largest)
+		  {
+			largest = data[i];
+			
+		  }
+		}	
+		System.out.println(largest);
 	}
-}
+  }
+  
